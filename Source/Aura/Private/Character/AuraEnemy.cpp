2ab597b01967c59ff10ed81c068e868e676a0354
Kalpu-24 <22amtics013@gmail.com>
2024-11-07 14:28:18 +0530

    HighLight Enemy

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -4,9 +4,9 @@
 #include "Character/AuraEnemy.h"
 
 void AAuraEnemy::HighlightActor(){
-	
+	bHighlighted = true;
 }
 
 void AAuraEnemy::UnHighlightActor(){
-	
+	bHighlighted = false;	
 }
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,9 +4,15 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/TargetInterface.h"
 
 AAuraPlayerController::AAuraPlayerController(){
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime){
+	Super::PlayerTick(DeltaTime);
+	CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay(){
@@ -42,5 +48,44 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue){
 	if (APawn* ControlledPawn = GetPawn<APawn>()){
 		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+void AAuraPlayerController::CursorTrace(){
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility,false,CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	/**
+	 * Line trace from cursor. There are several scenarios:
+	 *  A. LastActor is null && ThisActor is null
+	 *		- Do nothing
+	 *	B. LastActor is null && ThisActor is valid
+	 *		- Highlight ThisActor
+	 *	C. LastActor is valid && ThisActor is null
+	 *		- UnHighlight LastActor
+	 *	D. Both actors are valid, but LastActor != ThisActor
+	 *		- UnHighlight LastActor, and Highlight ThisActor
+	 *	E. Both actors are valid, and are the same actor
+	 *		- Do nothing
+	 */
+	if (LastActor == nullptr){
+		if (ThisActor != nullptr){
+			ThisActor->HighlightActor();
+		}
+	}
+	else{
+		if (ThisActor == nullptr){
+			LastActor->UnHighlightActor();
+		}
+		else {
+			if (LastActor != ThisActor){
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+		}
 	}
 }

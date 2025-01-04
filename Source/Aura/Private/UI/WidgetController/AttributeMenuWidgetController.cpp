b0ff88eb6fb83449d99a9a273e8bf4d5cd0b6a8f
Kalpu-24 <22amtics013@gmail.com>
2025-01-04 12:23:49 +0530

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright 2025 Kalp Games, All rights reserved.
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadCastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindLambdasToDependencies()
+{
+}
---
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,19 @@
+// Copyright 2025 Kalp Games, All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+public:
+	virtual void BroadCastInitialValues() override;
+	virtual void BindLambdasToDependencies() override;
+};

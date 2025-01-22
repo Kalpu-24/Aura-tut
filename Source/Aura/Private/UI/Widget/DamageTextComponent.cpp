a780d86d82e1a032ec68a1b1ac6dbe519ab0f779
Kalpu-24 <22amtics013@gmail.com>
2025-01-22 20:42:31 +0530

    Floating Text Widget

---
+++ Source/Aura/Private/UI/Widget/DamageTextComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright 2025 Kalp Games, All rights reserved.
+
+
+#include "UI/Widget/DamageTextComponent.h"
+
---
+++ Source/Aura/Public/UI/Widget/DamageTextComponent.h
@@ -0,0 +1,20 @@
+// Copyright 2025 Kalp Games, All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "DamageTextComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UDamageTextComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void SetDamageText(float Damage);
+};

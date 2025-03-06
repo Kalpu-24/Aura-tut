34f444edd514053f2945be250cbb76135c063bf3
Kalpu-24 <22amtics013@gmail.com>
2025-03-06 15:15:10 +0530

    Shaman Summon Locations

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -0,0 +1,23 @@
+// Copyright 2025 Kalp Games, All rights reserved.
+
+
+#include "AbilitySystem/Abilities/AuraSummonAbility.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
+{
+	const FVector ForwardVector = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const float DeltaSpread = SpawnSpread / NumMinions;
+
+	const FVector LeftOfSpread = ForwardVector.RotateAngleAxis(-SpawnSpread/2.f, FVector::UpVector);
+	TArray<FVector> SpawnLocations;
+	for (int32 i = 0; i < NumMinions; i++)
+	{
+		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+		FVector ChosenLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+		SpawnLocations.Add(ChosenLocation);
+	}
+	return SpawnLocations;
+}
